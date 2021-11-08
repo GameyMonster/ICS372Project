6ca13ac976c100bbbf615d96bb5827d97c0bfba3
drode73 <80714092+drode73@users.noreply.github.com>
2021-11-08 17:49:49 -0600

    Update AutomatedTester.java
    changed Product IDs to numbers

--- ICS372 GroupProject/edu.ics372.grocerystore.business/org/oobook/grocerystore/business/tests/AutomatedTester.java
+++ ICS372 GroupProject/edu.ics372.grocerystore.business/org/oobook/grocerystore/business/tests/AutomatedTester.java
@@ -1,6 +1,8 @@
 package org.oobook.grocerystore.business.tests;
 
 import edu.ics372.grocerystore.business.entities.Member;
+import edu.ics372.grocerystore.business.entities.Product;
+import edu.ics372.grocerystore.business.entities.ProductList;
 import edu.ics372.grocerystore.business.facade.GroceryStore;
 import edu.ics372.grocerystore.business.facade.Request;
 import edu.ics372.grocerystore.business.facade.Result;
@@ -15,7 +17,7 @@ public class AutomatedTester {
 	private final String[] membersID = { "i1", "i2", "i3", "i4", "i5", "i6", "i7" };
 	private final String[] productName = { "product1", "product2", "product3", "product4", "product5", "product6",
 			"product7" };
-	private final String[] productID = { "p1", "p2", "p3", "p4", "p5", "p6", "p7" };;
+	private final String[] productID = { "0", "1", "2", "3", "4", "5", "6" };
 	private final String[] productPrice = { "2.50", "30.00", "16.10", "21.50", "16.99", "2.99", "19.99" };
 	private final String[] newPrice = { "6.99", "2.00", "20.15", "12.00", "0.99", "5.13", "14.69" };
 	private final String[] reOrderLevel = { "22", "16", "50", "2", "15", "25", "33" };
@@ -73,16 +75,27 @@ public class AutomatedTester {
 	 * Tests shipments of the orders
 	 */
 	public void testShipment() {
+		addProducts();
 		for (int count = 0; count < 3; count++) {
+			System.out.println("count: " + count);
 			Request.instance().setProductID(productID[count]);
+			//Request.instance().setProductID("0");
 			Request.instance().setProductStock(Integer.toString(Integer.parseInt(reOrderLevel[count]) * 2));
+			//public Product(String name, int reorderLevel, int stock, double price) {
+			//ProductList.getInstance().insertProduct(new Product(productName[count],Integer.valueOf(reOrderLevel[count]),0,Double.valueOf(productPrice[count])));
 			Result result = GroceryStore.instance().processShipment(Request.instance());
 			assert result.getProductName().equals(productName[count]);
 			assert result.getProductReorderLevel().equals(reOrderLevel[count]);
 			assert result.getProductPrice().equals(productPrice[count]);
+			System.out.println("OK");
 		}
 	}
 
+	public void addProducts() {
+		for (int count = 0; count < 7; count++) {
+			ProductList.getInstance().insertProduct(new Product(productName[count],Integer.valueOf(reOrderLevel[count]),0,Double.valueOf(productPrice[count])));
+		}
+	}
 	/**
 	 * Test the ProductInfo
 	 */
